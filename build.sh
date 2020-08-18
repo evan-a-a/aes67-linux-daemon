c453ce31f36d58a9c23ea0b8d0f88b91eb6a1df7
Evan Anderson <evan@eaanderson.com>
2020-08-17 22:28:36 -0500

    build: Switch 3rd party deps to submodules
    Submodules are a cleaner way to track 3rd party repositories
    as opposed to using build scripts and .gitignore

--- build.sh
+++ build.sh
@@ -8,10 +8,7 @@ export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
 cd 3rdparty
-if [ ! -d ravenna-alsa-lkm.git ]; then
-  git clone https://bitbucket.org/MergingTechnologies/ravenna-alsa-lkm.git
-  cd ravenna-alsa-lkm
-  git checkout 35c708f3747474130790cf508c064360a9589ac8
+if [ ! -d ravenna-alsa-lkm ]; then
   echo "Apply patches to ravenna-alsa-lkm module ..."
   git apply ../patches/ravenna-alsa-lkm-kernel-v5.patch
   git apply ../patches/ravenna-alsa-lkm-enable-loopback.patch
@@ -26,18 +23,10 @@ if [ ! -d ravenna-alsa-lkm.git ]; then
   make
   cd ../..
 fi
-
-if [ ! -d cpp-httplib.git ]; then
-  git clone https://github.com/yhirose/cpp-httplib.git
-  cd cpp-httplib
-  git checkout 42f9f9107f87ad2ee04be117dbbadd621c449552
-  cd ..
-fi
 cd ..
 
 cd webui
 echo "Building and installing webui ..."
-#npm install react-modal react-toastify react-router-dom
 npm install
 npm run build
 cd ..
